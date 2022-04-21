b14825824b02d64f3908351036902f504db2bb76
ChaosOfThe4 <89749221+ChaosOfThe4@users.noreply.github.com>
2022-04-20 22:03:14 -0400

    Create Squad - Semi Auto.lua
    Anti-Recoil script for logitech mice for squad shooting semi auto.

---
+++ Squad - Semi Auto.lua
@@ -0,0 +1,12 @@
+EnablePrimaryMouseButtonEvents(true)
+
+function OnEvent(event, arg)
+  if IsKeyLockOn("numlock") then
+    if IsMouseButtonPressed(3) then
+      if (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
+        Sleep(50)
+        MoveMouseRelative(0,8)
+      end
+    end 
+  end
+end
