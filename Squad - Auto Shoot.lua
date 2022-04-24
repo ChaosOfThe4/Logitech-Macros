854c1e0478e6d69908874b9ad4004b2ddb597328
ChaosOfThe4 <89749221+ChaosOfThe4@users.noreply.github.com>
2022-04-24 19:47:10 -0400

    Update and rename Squad - Auto Shoot to Squad - Auto Shoot.lua

--- Squad - Auto Shoot.lua
+++ Squad - Auto Shoot.lua
@@ -7,12 +7,16 @@ function OnEvent(event, arg)
         if IsMouseButtonPressed(1) then
           repeat
             PressMouseButton(1)
-            Sleep(10)
+            Sleep(5)
             ReleaseMouseButton(1)
             MoveMouseRelative(0,math.random(1, 2))
           until not IsMouseButtonPressed(1)
+        else
+          Sleep(2)
         end
         until not IsMouseButtonPressed(3)
+      else
+        Sleep(2)
     end 
   end
 end
