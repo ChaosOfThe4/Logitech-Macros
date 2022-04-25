900b164f1d67f762b2437d09359ebcd02699fd59
ChaosOfThe4 <89749221+ChaosOfThe4@users.noreply.github.com>
2022-04-24 20:02:22 -0400

    Update Squad - Auto Shoot.lua

--- Squad - Auto Shoot.lua
+++ Squad - Auto Shoot.lua
@@ -4,19 +4,21 @@ function OnEvent(event, arg)
   if IsKeyLockOn("numlock") then
     if IsMouseButtonPressed(3) then
       repeat
+        --while IsMouseButtonPressed(1) do
         if IsMouseButtonPressed(1) then
           repeat
             PressMouseButton(1)
-            Sleep(5)
+            Sleep(1)
+            MoveMouseRelative(0,1)
             ReleaseMouseButton(1)
-            MoveMouseRelative(0,math.random(1, 2))
+            MoveMouseRelative(0,1)
           until not IsMouseButtonPressed(1)
         else
-          Sleep(2)
+          Sleep(1)
         end
         until not IsMouseButtonPressed(3)
       else
-        Sleep(2)
+        Sleep(1)
     end 
   end
 end
